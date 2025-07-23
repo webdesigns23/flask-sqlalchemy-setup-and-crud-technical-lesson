ef8c72a1b8df8d7d45a11b22c4c5b1161af60748
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-07-23 16:15:20 -0600

    final solution

--- server/models.py
+++ server/models.py
@@ -13,6 +13,9 @@ db = SQLAlchemy(metadata=metadata)
 class Pet(db.Model):
     __tablename__ = 'pets'
 
-    id = db.Column(db.Integer)
-    name = db.Column(db.String)
-    species = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    species = db.Column(db.String(50))
+
+    def __repr__(self):
+        return f"<Pet {self.id}, {self.name}, {self.species}>"
